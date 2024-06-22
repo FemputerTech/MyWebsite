66043cb8fdd4e3de041c14089f233e6a8621a3e7
FemputerTech <mleicht92@gmail.com>
2024-06-22 12:35:06 -0700

    implemented the basic setup of the web application

---
+++ index.py
@@ -0,0 +1,27 @@
+"""
+This module defines the view for handling GET requests to the home page.
+"""
+from flask import render_template
+from flask.views import MethodView
+
+
+class Index(MethodView):
+    """
+    Handles the index (home) page.
+
+    Methods:
+    -------
+    get(): Handles GET requests to the index (home) page.
+    """
+    
+    
+    def get(self):
+        """
+        Renders the index (home) page.
+        
+        Returns:
+        --------
+        response : str
+            The rendered HTML template for the index page.
+        """
+        return render_template("index.html")
