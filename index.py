1a502e73d385122515d54d3e333ab703d530fec0
FemputerTech <mleicht92@gmail.com>
2024-07-19 13:19:07 -0700

    added html files and rendered the index.html template from backend

--- index.py
+++ index.py
@@ -6,6 +6,21 @@ from flask.views import MethodView
 
 
 class Index(MethodView):
-   
+    """
+    Handles the index (home) page.
+
+    Methods:
+    -------
+    get(): Handles GET requests to the index (home) page.
+    """
     def get(self):
-        return {"details":"It's aliiiiiive!"}
+        """
+        Renders the index (home) page.
+        
+        Returns:
+        --------
+        response : str
+            The rendered HTML template for the index page.
+        """
+        print ("It's aliiiiive!")
+        return render_template('index.html')
