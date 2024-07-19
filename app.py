c07a1b2332a3a298c99f4a31df636891379f7627
FemputerTech <mleicht92@gmail.com>
2024-07-19 13:30:00 -0700

    setup my endpoints

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ To run the application, execute this script.
 """
 import flask
 from index import Index
+from about import About
+from contact import Contact
 
 
 # Initializes the Flask application
@@ -14,6 +16,20 @@ app = flask.Flask(__name__)
 # URL routing for the main landing page
 app.add_url_rule('/',
                  view_func=Index.as_view('index'),
+                 methods=['GET']
+                 )
+
+
+# URL routing for the about page
+app.add_url_rule('/about',
+                 view_func=About.as_view('about'),
+                 methods=['GET']
+                 )
+
+
+# URL routing for the contact page
+app.add_url_rule('/contact',
+                 view_func=Contact.as_view('contact'),
                  methods=['GET']
                  )
 
