18ec28981aa1c1c5f01abdf1a5f9933d09eee69c
FemputerTech <mleicht92@gmail.com>
2024-07-19 16:54:46 -0700

    added the data from my projects

---
+++ data.py
@@ -0,0 +1,18 @@
+projects = [
+    {
+        'title': "SignAi",
+        'tagline': "AI, React, Inclusive",
+        'description': "A sign language AI image classification tool.",
+        'image': "",
+        'link': "https://signai-rqc3nken5a-uw.a.run.app/",
+        'github': "https://github.com/FemputerTech/SignLanguageAI"
+    },
+    {
+        'title': "beet.",
+        'tagline': "RESTful API, Flask, Intuitive",
+        'description': "A recipe finder & meal planner application.",
+        'image': "",
+        'link': "https://mealplanner-luizsfhgza-uw.a.run.app/",
+        'github': "https://github.com/FemputerTech/MealPlanner",
+    }, 
+]
