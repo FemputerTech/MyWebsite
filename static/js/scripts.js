b15a79c6a6b321a4e593730766389e612fb5ea68
FemputerTech <mleicht92@gmail.com>
2024-08-28 11:24:07 -0700

    setup for dark mode and light mode

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,3 +4,20 @@ function scrollToSection(sectionId) {
     section.scrollIntoView({ behavior: "smooth" });
   }
 }
+
+function toggleTheme() {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  if (currentTheme === "dark") {
+    document.documentElement.removeAttribute("data-theme");
+    // Optionally change the icon to the moon (or another dark theme indicator)
+    document
+      .querySelector(".theme-toggle box-icon")
+      .setAttribute("name", "sun");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    // Optionally change the icon to the sun (or another light theme indicator)
+    document
+      .querySelector(".theme-toggle box-icon")
+      .setAttribute("name", "moon");
+  }
+}
