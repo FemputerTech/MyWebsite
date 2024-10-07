01eafd8942c08f751cfb3e51b85bf66045040f38
FemputerTech <mleicht92@gmail.com>
2024-10-07 14:33:26 -0700

    dynamically adding projects from the projects.json

---
+++ src/js/components/work.js
@@ -0,0 +1,38 @@
+// work.js
+import projects from "../../projects.json";
+import "../../styles/components/work.css";
+
+const renderProjects = () => {
+  const projectsList = document.querySelector(".projects-list");
+  projectsList.innerHTML = "";
+  projects.forEach((project) => {
+    const projectDiv = document.createElement("div");
+    const imagePath = require(`../../assets/project_images/${project.image}`);
+    projectDiv.className = "project";
+    projectDiv.innerHTML = `
+          <div class="project-image">
+            <a
+              href="${project.link}"
+              target="_blank"
+            >
+              <img src="${imagePath}" id="${project.id}" aria-label="${
+      project.label
+    }" />
+            </a>
+          </div>
+          <div class="project-content">
+            <h2 class="project-title">${project.title}</h2>
+            <h3>Technologies: <span class="technologies">${project.technologies.join(
+              ", "
+            )}</span></h3>
+            <p>${project.description}</p>
+            <a class="github-link" href="${
+              project.github
+            }" target="_blank">GitHub</a>
+          </div>
+    `;
+    projectsList.appendChild(projectDiv);
+  });
+};
+
+renderProjects();
