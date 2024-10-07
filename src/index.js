3666ecf3417565ddbd6c4c73e31b5b0f48930b4d
FemputerTech <mleicht92@gmail.com>
2024-10-07 14:32:24 -0700

    added the work.js to index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import "./styles/components/contact.css";
 import "./styles/components/goo.css";
 import "./styles/components/hero.css";
 import "./styles/components/skills.css";
-import "./styles/components/work.css";
 
 import "./js/components/navbar.js";
 import "./js/components/border.js";
+import "./js/components/work.js";
 // import "./js/darkmode.js";
 import heroPNG from "./assets/hero.png";
 // import resumePDF from "./assets/Meghan_Leicht_Resume_Web_Developer.pdf";
