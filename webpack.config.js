01a5a596168d86060b7b194a02c65d22181d7887
FemputerTech <mleicht92@gmail.com>
2024-10-07 16:29:10 -0700

    using the MiniCssExtractPlugin instead of style-loader to fix the FOUC issue. I needed to bundle my css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -27,7 +28,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|pdf)$/i,
@@ -48,6 +49,9 @@ module.exports = {
           to: path.resolve(__dirname, "dist"),
         },
       ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
     }),
   ],
 };
