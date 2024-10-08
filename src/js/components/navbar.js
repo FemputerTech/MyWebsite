352958e6788a3861ef240ace6910cc9e39e4db87
FemputerTech <mleicht92@gmail.com>
2024-10-08 13:31:26 -0700

    made my navbar links look cool

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -22,3 +22,19 @@ const animatedGhostOverlay = document.querySelector(".animated-ghost-overlay");
 animatedGhostOverlay.addEventListener("animationend", function () {
   this.style.display = "none"; // Hide after animation ends, otherwise navbar will be completely covered
 });
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+function resetStyles(link) {
+  link.style.boxShadow = "4px 4px 1px rgba(0, 0, 0, 1)";
+  link.style.transform = "translate(0, 0)";
+}
+
+navLinks.forEach((link) => {
+  link.addEventListener("mousedown", () => {
+    link.style.boxShadow = "none";
+    link.style.transform = "translate(2px, 2px)";
+  });
+  link.addEventListener("mouseup", () => resetStyles(link));
+  link.addEventListener("mouseleave", () => resetStyles(link));
+});
