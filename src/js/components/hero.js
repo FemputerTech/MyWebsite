f7e6d8c4b783cdefda87c2f1fc533190f2c59aad
FemputerTech <mleicht92@gmail.com>
2024-10-08 11:29:08 -0700

    adjusted the layout styling

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -6,9 +6,7 @@ import "../../styles/components/hero.css";
 const heroImageMeghan = document.getElementById("hero-image-meghan");
 heroImageMeghan.src = heroPNG;
 
-// import resumePDF from "./assets/Meghan_Leicht_Resume_Web_Developer.pdf";
-
-const heroButton = document.querySelector(".hero-button");
+const heroButton = document.getElementById("hero-button");
 heroButton.addEventListener("click", () => {
   window.open(resumePDF, "_blank"); // Opens the PDF in a new tab
 });
