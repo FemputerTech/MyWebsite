a5a2992bce6bc502a69c2408398da4e7dcddf245
FemputerTech <mleicht92@gmail.com>
2024-10-07 17:17:13 -0700

    fixed my resume button

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@
 import "./styles/components/about.css";
 import "./styles/components/contact.css";
 import "./styles/components/goo.css";
-import "./styles/components/hero.css";
 import "./styles/components/skills.css";
 
-import "./js/components/navbar.js";
 import "./js/components/border.js";
+import "./js/components/hero.js";
+import "./js/components/navbar.js";
 import "./js/components/work.js";
 // import "./js/darkmode.js";
-import heroPNG from "./assets/hero.png";
-// import resumePDF from "./assets/Meghan_Leicht_Resume_Web_Developer.pdf";
 import "./styles/main.css";
-
-const heroImageMeghan = document.getElementById("hero-image-meghan");
-heroImageMeghan.src = heroPNG;
--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -1,1 +1,14 @@
 // hero.js
+import heroPNG from "../../assets/hero.png";
+import resumePDF from "../../assets/Meghan_Leicht_Resume_Web_Developer.pdf";
+import "../../styles/components/hero.css";
+
+const heroImageMeghan = document.getElementById("hero-image-meghan");
+heroImageMeghan.src = heroPNG;
+
+// import resumePDF from "./assets/Meghan_Leicht_Resume_Web_Developer.pdf";
+
+const heroButton = document.querySelector(".hero-button");
+heroButton.addEventListener("click", () => {
+  window.open(resumePDF, "_blank"); // Opens the PDF in a new tab
+});
