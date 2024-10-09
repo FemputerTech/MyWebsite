3572604d2646608c37a54da07178199771fcb49d
FemputerTech <mleicht92@gmail.com>
2024-10-09 13:24:32 -0700

    instead of ball, calling it splatter

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -1,5 +1,19 @@
 // navbar.js
 // import "../../styles/components/navbar.css";
+const animatedGhostOverlay = document.querySelector(".animated-ghost-overlay");
+const navbarToggle = document.querySelector(".navbar-toggle");
+const header = document.querySelector("header");
+const topBorder = document.getElementById("top-border");
+const navLinks = document.querySelectorAll(".nav-link");
+const splatterSizes = [2, 4, 6];
+
+// ================== Animation ================== //
+animatedGhostOverlay.addEventListener("animationend", function () {
+  this.style.display = "none"; // Hide after animation ends, otherwise navbar will be completely covered
+});
+
+// ================== Toggle ================== //
+navbarToggle.addEventListener("click", toggleNavList);
 
 function toggleNavList() {
   const navList = document.querySelector(".nav-list");
@@ -11,52 +25,7 @@ function toggleNavList() {
   }
 }
 
-const navbarToggle = document.querySelector(".navbar-toggle");
-navbarToggle.addEventListener("click", toggleNavList);
-
-const animatedGhostOverlay = document.querySelector(".animated-ghost-overlay");
-animatedGhostOverlay.addEventListener("animationend", function () {
-  this.style.display = "none"; // Hide after animation ends, otherwise navbar will be completely covered
-});
-
-const navLinks = document.querySelectorAll(".nav-link");
-
-function resetStyles(link) {
-  link.style.boxShadow = "4px 4px 1px rgba(0, 0, 0, 1)";
-  link.style.transform = "translate(0, 0)";
-}
-
-navLinks.forEach((link) => {
-  link.addEventListener("mousedown", () => {
-    link.style.boxShadow = "none";
-    link.style.transform = "translate(2px, 2px)";
-  });
-  link.addEventListener("mouseup", () => resetStyles(link));
-  link.addEventListener("mouseleave", () => resetStyles(link));
-  link.addEventListener("click", () => {
-    if (window.location.href === link.href) {
-      console.log("splatter!");
-      for (let i = 0; i < 3; i++) {
-        createBalls(link);
-      }
-    }
-  });
-});
-
-// Navbar Hide
-const header = document.querySelector("header");
-const topBorder = document.getElementById("top-border");
-
-const showNavbar = () => {
-  header.style.opacity = 1;
-  topBorder.style.opacity = 1;
-};
-
-const hideNavbar = () => {
-  header.style.opacity = 0;
-  topBorder.style.opacity = 0;
-};
-
+// ================== Navbar Hide ================== //
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 160) {
     header.style.position = "fixed";
@@ -75,29 +44,56 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// Navlink clicks
-const options = {
-  duration: 400,
+const showNavbar = () => {
+  header.style.opacity = 1;
+  topBorder.style.opacity = 1;
 };
 
-const ballSizes = [2, 4, 6];
+const hideNavbar = () => {
+  header.style.opacity = 0;
+  topBorder.style.opacity = 0;
+};
 
-const homeNavLink = document.getElementById("home-page");
+// ================== NavLinks ================== //
+navLinks.forEach((link) => {
+  link.addEventListener("mousedown", () => {
+    link.style.boxShadow = "none";
+    link.style.transform = "translate(2px, 2px)";
+  });
+  link.addEventListener("mouseup", () => resetStyles(link));
+  link.addEventListener("mouseleave", () => resetStyles(link));
+  link.addEventListener("click", () => {
+    if (window.location.href === link.href) {
+      console.log("splatter!");
+      for (let i = 0; i < 3; i++) {
+        createSplatter(link);
+      }
+    }
+  });
+});
 
-function createBalls(link) {
+// ~~~~~~~~~~~~~~~~~~~ mouseup & mouseleave ~~~~~~~~~~~~~~~~~~~ //
+function resetStyles(link) {
+  link.style.boxShadow = "4px 4px 1px rgba(0, 0, 0, 1)";
+  link.style.transform = "translate(0, 0)";
+}
+
+// ~~~~~~~~~~~~~~~~~~~ click ~~~~~~~~~~~~~~~~~~~ //
+function createSplatter(link) {
   const linkRect = link.getBoundingClientRect();
   const centerX = linkRect.left + linkRect.width / 2;
   const centerY = linkRect.top + linkRect.height / 2;
   const header = document.querySelector("header");
-  const ball = document.createElement("div");
-  ball.className = "ball";
-  const randomSize = ballSizes[Math.floor(Math.random() * ballSizes.length)];
-  ball.style.width = `${randomSize}px`;
-  ball.style.height = `${randomSize}px`;
-  ball.style.display = "block";
-  ball.style.left = `${centerX}px`;
-  ball.style.top = `${centerY}px`;
-  header.appendChild(ball);
+  const splatter = document.createElement("div");
+  splatter.className = "splatter";
+  const randomSize =
+    splatterSizes[Math.floor(Math.random() * splatterSizes.length)];
+  splatter.style.width = `${randomSize}px`;
+  splatter.style.height = `${randomSize}px`;
+  splatter.style.display = "block";
+  splatter.style.left = `${centerX}px`;
+  splatter.style.top = `${centerY}px`;
+  header.appendChild(splatter);
   const x = Math.random() < 0.5 ? -1 : 1;
   const randomOffset = Math.random() * 5;
   const keyframes = [
@@ -127,8 +123,8 @@ function createBalls(link) {
       transform: `translate(${64 * x}px, ${-30 - randomOffset}px)`,
     }, // 100%
   ];
-  const animation = ball.animate(keyframes, options);
+  const animation = splatter.animate(keyframes, { duration: 400 });
   animation.onfinish = () => {
-    header.removeChild(ball);
+    header.removeChild(splatter);
   };
 }
