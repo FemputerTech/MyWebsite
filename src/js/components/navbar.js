410757de5b99514e4896361a542483e5cc135721
FemputerTech <mleicht92@gmail.com>
2024-10-09 13:12:50 -0700

    having the splatter apply to every navlink and moved the css in the navbar css

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -33,6 +33,14 @@ navLinks.forEach((link) => {
   });
   link.addEventListener("mouseup", () => resetStyles(link));
   link.addEventListener("mouseleave", () => resetStyles(link));
+  link.addEventListener("click", () => {
+    if (window.location.href === link.href) {
+      console.log("splatter!");
+      for (let i = 0; i < 3; i++) {
+        createBalls(link);
+      }
+    }
+  });
 });
 
 // Navbar Hide
@@ -68,10 +76,6 @@ window.addEventListener("scroll", () => {
 });
 
 // Navlink clicks
-function getRandomAdjustment() {
-  return Math.random() < 0.5 ? -1 : 0; // Randomly returns -1 or 0
-}
-
 const options = {
   duration: 400,
 };
@@ -80,20 +84,11 @@ const ballSizes = [2, 4, 6];
 
 const homeNavLink = document.getElementById("home-page");
 
-homeNavLink.addEventListener("click", () => {
-  if (window.location.href === homeNavLink.href) {
-    console.log("splatter!");
-    for (let i = 0; i < 3; i++) {
-      createBalls();
-    }
-  }
-});
-
-function createBalls() {
-  const linkRect = homeNavLink.getBoundingClientRect();
+function createBalls(link) {
+  const linkRect = link.getBoundingClientRect();
   const centerX = linkRect.left + linkRect.width / 2;
   const centerY = linkRect.top + linkRect.height / 2;
-  const balls = document.querySelector(".balls");
+  const header = document.querySelector("header");
   const ball = document.createElement("div");
   ball.className = "ball";
   const randomSize = ballSizes[Math.floor(Math.random() * ballSizes.length)];
@@ -102,38 +97,38 @@ function createBalls() {
   ball.style.display = "block";
   ball.style.left = `${centerX}px`;
   ball.style.top = `${centerY}px`;
-  balls.appendChild(ball);
+  header.appendChild(ball);
   const x = Math.random() < 0.5 ? -1 : 1;
-  const randomYOffset = Math.random() * 5;
+  const randomOffset = Math.random() * 5;
   const keyframes = [
     { opacity: 1, transform: `translateX(${0 * x}) translatey(0)` }, // 0%
     {
       opacity: 0,
-      transform: `translate(${2 * x}px, ${-10 - randomYOffset}px)`,
+      transform: `translate(${2 * x}px, ${-10 - randomOffset}px)`,
     }, // 10%
     {
       opacity: 1,
-      transform: `translate(${4 * x}px, ${-20 - randomYOffset}px)`,
+      transform: `translate(${4 * x}px, ${-20 - randomOffset}px)`,
     }, // 20%
     {
       opacity: 1,
-      transform: `translate(${8 * x}px, ${-30 - randomYOffset}px)`,
+      transform: `translate(${8 * x}px, ${-30 - randomOffset}px)`,
     }, // 30%
     {
       opacity: 1,
-      transform: `translate(${16 * x}px, ${-40 - randomYOffset}px)`,
+      transform: `translate(${16 * x}px, ${-40 - randomOffset}px)`,
     }, // 40%
     {
       opacity: 1,
-      transform: `translate(${32 * x}px, ${-50 - randomYOffset}px)`,
+      transform: `translate(${32 * x}px, ${-50 - randomOffset}px)`,
     }, // 50%
     {
       opacity: 0,
-      transform: `translate(${64 * x}px, ${-30 - randomYOffset}px)`,
+      transform: `translate(${64 * x}px, ${-30 - randomOffset}px)`,
     }, // 100%
   ];
   const animation = ball.animate(keyframes, options);
   animation.onfinish = () => {
-    balls.removeChild(ball);
+    header.removeChild(ball);
   };
 }
